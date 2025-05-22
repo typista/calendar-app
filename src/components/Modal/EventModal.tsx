b7266afb037c0a5cdc18773d79e3dfa7fbbc5181
Kizuka Makoto <typista@gmail.com>
2025-05-22 10:08:22 +0900

    予定を追加モーダルの保存ボタンのdisabledを廃止

--- src/components/Modal/EventModal.tsx
+++ src/components/Modal/EventModal.tsx
@@ -101,9 +101,8 @@ type EventModalProps = {
                   キャンセル
                 </button>
                 <button
-                  className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 disabled:opacity-50"
+                  className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600:opacity-50"
                   onClick={onClick}
-                  disabled={!newEventTitle.trim()}
                 >
                   保存
                 </button>
